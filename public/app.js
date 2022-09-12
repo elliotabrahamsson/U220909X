491351332f7c9f143d1f571fe7c5ffee8045965c
Elliot <elliotsson@outlook.com>
2022-09-12 13:49:00 +0200

    starting build in js, navbar

--- public/app.js
+++ public/app.js
@@ -1,2 +1,16 @@
 console.log('I am alive!');
+build();
 
+function build() {
+    const Header = document.createElement('div');
+    
+    Header.className ='navbar navbar-expand bg-primary';
+    app.appendChild(Header);
+
+    const Ommig = document.createElement('h1');
+    Ommig.innerHTML = 'Om mig'
+    Ommig.className = 'font-monospace fs-4';
+    Header.appendChild(Ommig);
+
+    const 
+} 

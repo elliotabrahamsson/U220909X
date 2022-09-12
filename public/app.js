2edaffa23cf30e09eb411ec281d5608d36746ce8
Elliot <elliotsson@outlook.com>
2022-09-12 14:12:57 +0200

    added picture and div tags

--- public/app.js
+++ public/app.js
@@ -12,5 +12,21 @@ function build() {
     Ommig.className = 'font-monospace fs-4';
     Header.appendChild(Ommig);
 
-    const 
+    const bildDiv = document.createElement('div');
+    bildDiv.setAttribute('id', 'Bild');
+    app.appendChild(bildDiv);
+
+    const Bild = document.createElement('img');
+    Bild.setAttribute('src', 'dog.jpg');
+    bildDiv.appendChild(Bild);
+
+    const RutaDiv = document.createElement('div');
+    RutaDiv.setAttribute('id', 'Ruta');
+    app.appendChild(RutaDiv);
+
+    const MinsommarRuta = document.createElement('p');
+    MinsommarRuta.innerHTML = 'jadu';
+    MinsommarRuta.className = 'bg-primary container-sm';
+    RutaDiv.appendChild(MinsommarRuta);
+
 } 
